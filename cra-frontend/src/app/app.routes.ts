7ad0c1215698f4d4d434da3816e6b8573f7a87ef
Richmond GUEHI <richmondb.guehi@gmail.com>
2026-03-19 22:11:55 +0100

    add component Dashboard

--- cra-frontend/src/app/app.routes.ts
+++ cra-frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { User } from './components/user/user';
 import { Login } from './components/login/login';
 import { authGuard } from './guards/auth.guard';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
     { path: 'login', component: Login },
@@ -10,6 +11,11 @@ export const routes: Routes = [
         component : User,
         //active le guard
         canActivate: [authGuard]
+    },
+    { 
+        path: 'dashboard', 
+        component: Dashboard, 
+        canActivate: [authGuard] 
     },
 
     // default
--- cra-frontend/src/app/components/dashboard/dashboard.ts
+++ cra-frontend/src/app/components/dashboard/dashboard.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UserService } from '../../services/user-service';
+import { User } from '../user/user';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+  private userService = inject(UserService)
+
+  totalCollaborateurs = signal<number>(0);
+  actifsCount =signal<number>(0);
+
+
+  ngOnInit(): void {
+    this.loadStats();
+  }
+
+  loadStats(){
+    this.userService.getAllUsers().subscribe({
+      next: (users) => {
+        this.totalCollaborateurs.set(users.length);
+        
+        this.actifsCount.set(users.filter( u => u.active).length);
+      },
+
+      error: (err) => console.error('Erreur lors du chargement des stats', err)
+    })
+  }
+
+
+
 
 }
